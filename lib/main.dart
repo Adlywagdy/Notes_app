b4a69e6e9e05ee2518495db77eb20d58eca526cd
Adly Wagdy <adly@example.com>
2025-08-15 03:28:01 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/pages/Notespage.dart';
+
+void main() {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: ThemeData.dark(), home: NotesPage());
+  }
+}
